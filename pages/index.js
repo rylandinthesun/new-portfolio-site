63d992770d4fbe8ed14f76dac2bcb8baa5b26ff1
Ryland Oehlers <ryland.oehlers@gmail.com>
2021-08-12 12:37:01 -0400

    summary added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import selfPortrait from '../images/selfportrait.jpg';
+import { FiMail } from 'react-icons/fi';
+import { FaLinkedin, FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa';
 import styles from '../styles/Home.module.css';
 
 export default function Home () {
@@ -23,6 +25,45 @@ export default function Home () {
 					objectFit="cover"
 				/>
 			</main>
+
+			<section className={styles.summary}>
+				<div>
+					<h2>S</h2>
+					<h2>U</h2>
+					<h2>M</h2>
+					<h2>M</h2>
+					<h2>A</h2>
+					<h2>R</h2>
+					<h2>Y</h2>
+				</div>
+				<div>
+					<p>
+						Full-Stack Software Engineer who was a Full-Time Touring Musician for 10+ years. Specializing in
+						Front-End Development, I am passionate about bringing together creative designs with code.
+					</p>
+					<section className={styles.socials}>
+						<a
+							href="mailto:ryland.oehlers@gmail.com?subject=Website Inquiry"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<FiMail />
+						</a>
+						<a href="https://www.linkedin.com/in/rylandoehlers" target="_blank" rel="noopener noreferrer">
+							<FaLinkedin />
+						</a>
+						<a href="https://www.github.com/rylandinthesun" target="_blank" rel="noopener noreferrer">
+							<FaGithub />
+						</a>
+						<a href="https://www.instagram.com/rylandinthesun" target="_blank" rel="noopener noreferrer">
+							<FaInstagram />
+						</a>
+						<a href="https://www.twitter.com/rylandinthesun" target="_blank" rel="noopener noreferrer">
+							<FaTwitter />
+						</a>
+					</section>
+				</div>
+			</section>
 		</div>
 	);
 }
