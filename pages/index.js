7b59380a3952ab4781765747c05b41def47b5909
Ryland Oehlers <ryland.oehlers@gmail.com>
2021-08-25 15:12:39 -0400

    fetch link change

--- pages/index.js
+++ pages/index.js
@@ -315,7 +315,7 @@ const Home = ({ projects }) => {
 export default Home;
 
 export const getStaticProps = async () => {
-	const res = await fetch('https://portfolio-website-woad-nine.vercel.app/api/projects');
+	const res = await fetch('https://rylandoehlers.com/api/projects');
 	const projects = await res.json();
 
 	if (!projects) {
