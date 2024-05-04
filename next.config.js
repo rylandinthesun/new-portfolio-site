948bbfcee4b63aaac282b5f597619520fc297231
Ryland Oehlers <ryland.oehlers@gmail.com>
2024-05-04 00:10:40 -0700

    big ass changes

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	reactStrictMode : true,
-
-	images          : {
-		domains : [
-			'i.postimg.cc'
-		]
-	}
-};
--- pages/api/projects/index.js
+++
@@ -1,5 +0,0 @@
-import { projectsData } from '../../../projectsData.js';
-
-export default async (req, res) => {
-	res.status(200).json(projectsData);
-};
